d33b7bf0b57c748d719d4282ab004fec25885e57
patricknolandev <patricknolan90@gmail.com>
2025-06-12 15:34:48 +1000

    Character movement, projectile setup
    - Added dark souls style movement to character
    - Created class for projectile attack

--- Source/ActionRoguelike/Private/TutCharacter.cpp
+++ Source/ActionRoguelike/Private/TutCharacter.cpp
@@ -2,6 +2,7 @@
 
 #include "TutCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -10,11 +11,18 @@ ATutCharacter::ATutCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Attach a spring arm component to the character
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->bUsePawnControlRotation = true;
 	SpringArmComp->SetupAttachment(RootComponent);
-	
+	// Attach a camera component to the spring arm so its controlled by it
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	// Get the character to rotate towards the direction of acceleration
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// Disable the character rotating towards the yaw of the player controller 
+	bUseControllerRotationYaw = false;
 }
 
 // Called when the game starts or when spawned
@@ -22,11 +30,6 @@ void ATutCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-void ATutCharacter::MoveForward(float Value)
-{
-	AddMovementInput(GetActorForwardVector(), Value);
 }
 
 // Called every frame
@@ -41,7 +44,41 @@ void ATutCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Set up the inputs for moving on XY axis
 	PlayerInputComponent->BindAxis("MoveForward", this, &ATutCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ATutCharacter::MoveRight);
+	
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
+void ATutCharacter::MoveForward(float Value) // Player should move forward in direction of camera
+{
+	// Store the controller rotation
+	FRotator ControlRot = GetControlRotation();
+	// Only get the yaw so we don't move up/down/roll based on camera
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+	
+	// Move in the horizontal direction of the camera
+	AddMovementInput(ControlRot.Vector(), Value);
+}
+
+void ATutCharacter::MoveRight(float Value) // Player should move left and right based on camera yaw
+{
+	// Store player controller rotation and only get yaw
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+
+	// X - Forward (Red)
+	// Y - Right (Green)
+	// Z - Up (Blue)
+
+	// Store the Y rotation of the player controller, in FVector RightVector, using FRotationMatrix constructor,
+	// to make a temporary matrix object, then call the method GetScaledAxis to convert the
+	// rotator into a vector, getting the Y vector
+	FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+	// Move right & left based on controller Y rotation
+	AddMovementInput(RightVector, Value);
+}
--- Source/ActionRoguelike/Public/TutCharacter.h
+++ Source/ActionRoguelike/Public/TutCharacter.h
@@ -30,6 +30,7 @@ protected:
 	virtual void BeginPlay() override;
 
 	void MoveForward(float Value);
+	void MoveRight(float X);
 
 public:	
 	// Called every frame
