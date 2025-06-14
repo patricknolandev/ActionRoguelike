a30f071f8b53efb2491a032649f7571e5b35ca32
patricknolandev <patricknolan90@gmail.com>
2025-06-14 16:43:25 +1000

    L4.1 Base Interface class add
    - Added base c++ class for gameplay interface

---
+++ Source/ActionRoguelike/Private/TutGameplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TutGameplayInterface.h"
+
+// Add default functionality here for any ITutGameplayInterface functions that are not pure virtual.
---
+++ Source/ActionRoguelike/Public/TutGameplayInterface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "TutGameplayInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UTutGameplayInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ACTIONROGUELIKE_API ITutGameplayInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
