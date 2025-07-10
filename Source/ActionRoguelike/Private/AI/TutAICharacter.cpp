b32f26e474878046df622072448e4ebfa75cd274
patricknolandev <patricknolan90@gmail.com>
2025-07-10 11:29:55 +1000

    L10. basic enemy AI walks into attack range in LOS
    - Created basic ranged enemy
    - Setup the AI framework for ranged enemy
    - AI logic for walking into attack range, checks whether player is in LOS

---
+++ Source/ActionRoguelike/Private/AI/TutAICharacter.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/TutAICharacter.h"
+
+// Sets default values
+ATutAICharacter::ATutAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATutAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATutAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
